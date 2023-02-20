f38d3ed92798ab84bc10788939293d45e565aa02
Hoja Tesema <hojats@hotmail.com>
2023-02-20 07:03:18 -0800

    main - Prints the last digit of a randomly generated number  * and whether it is greater than 5, less than 6, or 0.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * Main Entry point
+ *main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
  *
  * Return:Always 0
  */

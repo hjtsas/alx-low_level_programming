4fb74a5597f4fed3501b66ab9c8bfe7dbed457d3
Hoja Tesema <hojats@hotmail.com>
2023-02-23 12:29:56 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_numbers - print 0-9
- * Return: void
+ * return: void
  */
 
 void print_numbers(void)
 {
 	char c;
 
-	for (c = '0', c <= '9'; c++;)
+	for (c = '0', c <= '9'; c++)
 	{
 		_putchar(c);
 	}

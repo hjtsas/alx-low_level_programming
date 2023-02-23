2e42c3bd804afd648b8b8b5a75a3d323457d49c8
Hoja Tesema <hojats@hotmail.com>
2023-02-23 11:40:34 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * int _isupper - uppercase letter
+ * _isupper - uppercase letter
  * @c: char to be checked
  *
  * Return:0 or 1
  */
 
-int _insupper(int c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);

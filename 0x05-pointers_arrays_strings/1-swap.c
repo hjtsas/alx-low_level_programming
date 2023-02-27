c81ba2d5311c7bb882e1c55b1306734af18551c5
Hoja Tesema <hojats@hotmail.com>
2023-02-27 05:49:19 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of two int
+ *@a: int
+ *@b: int
+ *
+ * return (0)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x, y;
+
+	x = y;
+	y = temp;
+
+	_putchar('x = %d, y =%d\n', x, y);
+	return(0);
+}

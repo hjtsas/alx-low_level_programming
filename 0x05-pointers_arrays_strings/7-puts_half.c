439b6e2dec6cb0e45aae3bcd4a3e6f80c5152466
Hoja Tesema <hojats@hotmail.com>
2023-02-27 12:35:33 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,19 +3,21 @@
 /**
  * puts_half - print half of a string
  * @str: string
+ * longi: long int i
  * if odd len, n = (length_of_the_string - 1) / 2
  * return: half of the string
  */
 
 void puts_half(char *str)
 {
-	int a, n, i, longi;
+	int a, n, i;
 
-	longi = 0;
+	i = 0;
 
 	for (a = 0; str[a] != '\0'; a++)
 		i++;
 	n = (i / 2);
+
 	if ((i % 2) == 1)
 		n = ((i + 1) / 2);
 	for (a = n; str[a] != '\0'; a++)

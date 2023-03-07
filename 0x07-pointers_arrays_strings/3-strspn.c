28d11f9645f1f434f5d4d202a540cd8c73a0ef30
Hoja Tesema <hojats@hotmail.com>
2023-03-07 06:47:31 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,4 +10,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int n = 0;
+	int i;
 
+	while (*s)
+	{
+		for (i = 0; accept[i] < '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				n++;
+			}
+			else if (accept[i + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}

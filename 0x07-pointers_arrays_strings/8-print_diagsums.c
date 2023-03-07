95a198af9f08b891946544d6e980b8f989c22def
Hoja Tesema <hojats@hotmail.com>
2023-03-07 09:35:04 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -22,7 +22,7 @@ void print_diagsums(int *a, int size)
 	}
 	for (i = size - 1; i >= 0; i--)
 	{
-		s2 = a[i * size + (size - i - 1)];
+		s2 += a[i * size + (size - i - 1)];
 	}
 	printf("%d, %d\n", s1, s2);
 }

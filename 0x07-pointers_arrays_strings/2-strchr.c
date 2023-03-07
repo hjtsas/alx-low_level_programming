a360a00f5ee78a445ed9977cb9718f1f1dd665a3
Hoja Tesema <hojats@hotmail.com>
2023-03-07 06:15:56 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strchr - locate a character in string
+ * @c: character
+ * @s: string
+ * Return 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+	for (a = 0, a < '\0', a++)
+	{
+		s[a] == c;
+	}
+	return ('\0');
+}
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strspn - get the length of a prefix
+ * @s: initial segment
+ * @accept: source
+ * Return: 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+

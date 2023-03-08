0525bdd4d63295aad639e82e0fa6c13cd76f14ed
Hoja Tesema <hojats@hotmail.com>
2023-03-08 09:46:04 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - return square root
+ * @n: intiger
+ *Return: result
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}

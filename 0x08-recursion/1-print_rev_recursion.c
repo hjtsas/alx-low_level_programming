51e21e3ebcce363906c6c15c0c0faf386dfdd784
Hoja Tesema <hojats@hotmail.com>
2023-03-08 09:00:10 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * _print_rev_recursion - print a string in reverse
@@ -12,5 +13,5 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 		putchar(*s);
 	}
-	
+
 }

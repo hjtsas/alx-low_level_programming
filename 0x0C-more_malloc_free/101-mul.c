c4ba193a5d1e848fe88362505f040e92bd304988
Hoja Tesema <hojats@hotmail.com>
2023-03-17 00:49:08 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -88,10 +88,12 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
+	{
 		if (result[i])
 			a = 1;
 		if (a)
 			_putchar(result[i] + '0');
+	}
 	if (!a)
 		_putchar('0');
 	_putchar('\n');

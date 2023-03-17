e6f9909aac6f4f50cde8b5814d497c20d600ee55
Hoja Tesema <hojats@hotmail.com>
2023-03-17 00:37:28 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -63,23 +63,17 @@ int main(int argc, char *argv[])
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-	{
 		errors();
-	}
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
-	{
 		return (1);
-	}
 	for (i = 0; i <= len1 + len2; i++)
-	{
+
 		result[i] = 0;
-	}
 	for (len1 = len1 - 1; len1 >= 0; len1--)
-	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
@@ -91,7 +85,6 @@ int main(int argc, char *argv[])
 		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
-	}
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])

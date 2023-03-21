8832c450bc6018590380ef681000a883a540f682
Hoja Tesema <hojats@hotmail.com>
2023-03-21 00:25:39 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * *malloc_checked - allocates memory using malloc
@@ -12,8 +13,6 @@ void *malloc_checked(unsigned int b)
 
 	p = malloc(b);
 	if (p == NULL)
-	{
 		exit(98);
-	}
 	return (p);
 }

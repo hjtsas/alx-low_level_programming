03a7964ddbb3c3f2af8c68aa548b179897d9d7e1
Hoja Tesema <hojats@hotmail.com>
2023-03-22 08:47:54 -0700

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substruct numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplay numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - shows remender
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+
+}

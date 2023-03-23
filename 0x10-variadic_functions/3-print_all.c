76ce3be9bc85c6c726f40e2cfbd186f9d82518bc
Hoja Tesema <hojats@hotmail.com>
2023-03-23 11:00:55 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -32,11 +32,9 @@ void print_all(const char * const format, ...)
 				case 's':
 					str = va_arg(l, char *);
 					if (!str)
-					{
 						str = "(nil)";
 					printf("%s%s", sep, str);
-					break;
-					}
+					break;					}
 				default:
 					i++;
 					continue;

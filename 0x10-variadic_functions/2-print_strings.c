4e753b28590a6a87537c94d9b2afda633dd476fe
Hoja Tesema <hojats@hotmail.com>
2023-03-23 09:46:58 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers
+ * @separator: string to be printed
+ * @n: number of integers
+ * @....: variable numbers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list x;
+	unsigned int i;
+	char *str;
+
+	va_start(x, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(x, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+}

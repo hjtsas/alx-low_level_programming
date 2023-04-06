8b3928e7d7f8b7709d560d7ea049812d523f01a5
Hoja Tesema <hojats@hotmail.com>
2023-04-06 07:32:04 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an int
+ * @b: pointer to a string
+ * Return: b or NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int val = 0;
+    int i = 0;
+
+    if (b == NULL)
+        return (0);
+
+    while (b[i] == '0' || b[i] == '1')
+    {
+        val <<= 1;
+        val += b[i] - '0';
+        i++;
+    }
+
+    return (val);
+}

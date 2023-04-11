7beb472ca0e9c11c224b2fbe7ec1bd0a3a59a90a
Hoja Tesema <hojats@hotmail.com>
2023-04-11 13:00:54 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0'; i++);
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
 		if (write(fd, text_content, i) == -1)
 			return (-1);
 	}

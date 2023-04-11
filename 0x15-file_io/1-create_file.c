b22f668d05b341900a3c4234d44b55a1572b21de
Hoja Tesema <hojats@hotmail.com>
2023-04-11 12:48:31 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: NULL terminated string to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT
+			| O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len])len++;
+		if (write(fd, text_content, len) != len)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}

10314f593b2a4d1e74ddd8980b7a663e6594566f
Hoja Tesema <hojats@hotmail.com>
2023-04-11 13:03:02 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (text_content[len])len++;
+		while (text_content[len])
+			len++;
 		if (write(fd, text_content, len) != len)
 			return (-1);
 	}
